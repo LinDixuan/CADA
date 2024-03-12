728fcd8e42b31f9dcb89da2ae9246d4c35dd5fa1
LinDixuan <127588279+LinDixuan@users.noreply.github.com>
2024-03-12 12:54:31 +0800

    Update eval.bash

--- eval.bash
+++ eval.bash
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run --nproc_per_node=4 --master_port=25902 train_clip_cuhk.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run --nproc_per_node=4 --master_port=25902 train.py \
 --config ./configs/retrieval_icfg.yaml \
 --output_dir output/test \
 --batch_size_test 64 \
